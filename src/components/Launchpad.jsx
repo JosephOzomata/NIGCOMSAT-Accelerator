4d26bb17d1019ed90a6dab94f7a859f6486731bf
Destiny115-234 <destinyfelix010@gmail.com>
2026-07-16 23:57:21 +0100

    spacefest herosection and uploaded images

--- src/components/Launchpad.jsx
+++ src/components/Launchpad.jsx
@@ -1,9 +1,8 @@
 
-// import launchpadImage from "../assets/images/launchpad.png";
+import launchpadImage from "../images/launchpad.png";
 
 function Launchpad() {
   return (
-
     <section className="bg-[#F7F3EB] py-24 px-16">
       <div className="max-w-7xl mx-auto flex items-center gap-20">
         <div className="w-[55%]">
@@ -11,9 +10,11 @@ function Launchpad() {
             src={launchpadImage}
             alt="Launchpad"
             className="w-full h-[700px] object-cover rounded-[30px]"
-         />
+          />
 
-         <div className="w-[45%]">
+        </div>
+
+        <div className="w-[45%]">
           <p className="uppercase text-[#2166D1] tracking-widest font-semibold mb-3">
             NIGCOMSAT Accelerator
           </p>
@@ -35,8 +36,6 @@ function Launchpad() {
           <button className="bg-[#2166D1] text-white px-8 py-4 rounded-full hover:bg-blue-800 transition duration-300">
             Join the Accelerator
           </button>
-
-         </div>
 
         </div>
 
