252a648f712abb59f06f29a9cb1cc2b2961e4b1f
frankkator <frankkkator05gmail.com>
2026-07-17 00:08:40 +0100

    Spotlight

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './pages/Home'
+import Spotlight from './pages/SpotLightPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Home />
+      <Spotlight />
     </>
   )
 }
