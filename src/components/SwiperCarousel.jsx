e4430d641f42c87fe3b3de8beac343961bfc7fc2
frankkator <frankkkator05gmail.com>
2026-07-20 11:29:47 +0100

    trial

--- src/components/SwiperCarousel.jsx
+++ src/components/SwiperCarousel.jsx
@@ -3,7 +3,7 @@ import { Navigation,Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import "Swiper/css/pagination";
+import "swiper/css/pagination";
 
 const slide = [
        
