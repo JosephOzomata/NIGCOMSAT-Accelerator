70847f1b1690b5e629ab8d49513c916e68bd2aa0
JosephOzomata <ozomataadinoyi@gmail.com>
2026-07-20 16:40:42 +0100

    Small changes

--- src/pages/Spacefest.jsx
+++ src/pages/Spacefest.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import AboutSpaceFest from '../components/AboutSpaceFest'
+import SpotLightPage from '../components/SpotLightPage'
+import ClubScoring from '../components/ClubScoring'
 
 const Spacefest = () => {
   return (
     <>
       <AboutSpaceFest />
+      <ClubScoring />
+      <SpotLightPage />
     </>
   )
 }
