39d02aef238e320a3665edc276dab9912230aab6
damionaltair <awahchetachukwu@gmail.com>
2026-07-20 12:46:12 +0100

    club scoring in space fest

--- src/pages/Spacefest.jsx
+++ src/pages/Spacefest.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutSpaceFest from '../components/AboutSpaceFest'
+import ClubScoring from '../components/ClubScoring'
 
 const Spacefest = () => {
   return (
     <>
       <AboutSpaceFest />
+      <ClubScoring />
     </>
   )
 }
