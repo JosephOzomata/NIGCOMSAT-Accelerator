0175e0915e0ff0713ab7c142d1106bfbc81fe890
frankkator <frankkkator05gmail.com>
2026-07-29 23:40:16 +0100

    meh

--- src/pages/Spacefest.jsx
+++ src/pages/Spacefest.jsx
@@ -3,12 +3,7 @@ import HeroSection from '../components/HeroSection'
 import AboutSpaceFest from '../components/AboutSpaceFest'
 import SpotLightPage from '../components/SpotLightPage'
 import ClubScoring from '../components/ClubScoring'
-<<<<<<< HEAD
-
-=======
-// import HeroSection from '../components/HeroSection'
 import CompetitionFormat from '../components/CompetitionFormat'
->>>>>>> 20af2bba4523ea72b259ed8d0662bf37153196fb
 
 
 const Spacefest = () => {
