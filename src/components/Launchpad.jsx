223f354f355ee12d64ccff69b6418b1de90e7628
JOSEPHOZOMATA <ozomataadinoyi@gmail.com>
2026-07-30 12:47:07 +0100

    joseph's updates 3.0

--- src/components/Launchpad.jsx
+++ src/components/Launchpad.jsx
@@ -3,9 +3,9 @@ import launchpadImage from "../images/launchpad.png";
 
 function Launchpad() {
   return (
-    <section className="bg-gray-200 py-24 px-16">
-      <div className="max-w-7xl mx-auto flex items-center gap-20">
-        <div className="w-[55%]">
+    <section className="bg-gray-200 py-24 px-4">
+      <div className="max-w-7xl mx-auto lg:grid-cols-2 lg:grid sm:hidden gap-10">
+        <div className="w-full">
           <img
             src={launchpadImage}
             alt="Launchpad"
@@ -14,7 +14,7 @@ function Launchpad() {
 
         </div>
 
-        <div className="w-[45%]">
+        <div className="w-full">
           <p className="uppercase text-[#2166D1] tracking-widest font-semibold mb-3">
             NIGCOMSAT Accelerator
           </p>
@@ -36,6 +36,43 @@ function Launchpad() {
           <button className="bg-[#2166D1] text-white px-8 py-4 rounded-full hover:bg-blue-800 transition duration-300">
             Join the Accelerator
           </button>
+
+        </div>
+
+      </div>
+
+      <div className="max-w-7xl mx-auto grid lg:hidden sm:grid-cols-1 gap-10">
+        <div className="w-full">
+          <p className="uppercase text-[#2166D1] tracking-widest font-semibold mb-3">
+            NIGCOMSAT Accelerator
+          </p>
+
+          <h2 className="text-[60px] font-bold leading-tight text-[#2166D1] mb-6">
+            Build.
+            <br />
+            Launch.
+            <br />
+            Scale.
+          </h2>
+
+          <p className="text-xl text-gray-700 leading-9 mb-10">
+            More than an accelerator—we're a launchpad for ambitious founders,
+            providing the mentorship, partnerships, and national infrastructure
+            needed to transform bold ideas into lasting impact.
+          </p>
+
+          <button className="bg-[#2166D1] text-white px-8 py-4 rounded-full hover:bg-blue-800 transition duration-300">
+            Join the Accelerator
+          </button>
+
+        </div>
+
+        <div className="w-full">
+          <img
+            src={launchpadImage}
+            alt="Launchpad"
+            className="w-full h-[700px] object-cover rounded-[30px]"
+          />
 
         </div>
 
